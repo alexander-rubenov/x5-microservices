1580901f3918667a132a502bc09a961295a0c2da
Vadim Shkolenko <vadimand1998@gmail.com>
2019-12-01 02:50:23 +0300

    added search

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,14 +2,16 @@ import React from 'react'
 import Header from '../header'
 import { store } from '../../store'
 import { Provider } from 'react-redux'
-import ContentWrapper from '../content-wrapper'
+// import ContentWrapper from '../content-wrapper'
+import SearchMicroservices from '../search-microservices'
 import Footer from '../footer'
 
 const App = () => {
   return (
     <Provider store={store}>
       <Header />
-      <ContentWrapper />
+      {/* <ContentWrapper /> */}
+      <SearchMicroservices />
       <Footer />
     </Provider>
   )
