0de4e62c1d033e4688e638ad0f5eb5b1712b4393
Alexander Rubenov <alexander.rubenov@gmail.com>
2019-12-01 01:36:08 +0300

    changed logo

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import styles from './logo.module.css'
-import logo from '../../static/img/main-logo.svg'
+import logo from '../../static/img/header-logo.svg'
 
 const Logo = () => {
-    return (
-        <div className={styles.logo}>
-            <img src={logo} alt=""/>
-        </div>
-    )
+  return (
+    <div className={styles.logo}>
+      <img src={logo} alt="" />
+    </div>
+  )
 }
 
 export default Logo
