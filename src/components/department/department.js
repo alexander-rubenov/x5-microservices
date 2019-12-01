baeed359dbde546a3dae4b0e9187ee9be9b3baac
MatveyPower <55589736+MatveyPower@users.noreply.github.com>
2019-12-01 03:42:20 +0300

    added layout page 3

--- src/components/department/department.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Department = () => {
-    return (
-        <div>
-            department
-        </div>
-    )
-}
--- src/store/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import {combineReducers} from 'redux'
-import {departmentsReducer} from './departments'
-
-export const reducer = combineReducers({
-    departments: departmentsReducer,
-})
